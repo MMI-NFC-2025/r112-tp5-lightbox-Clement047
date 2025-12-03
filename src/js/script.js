72f74306ea28c0af9891966b8e7ae17aa211c831
Clement047 <clement.bertrand02@edu.univ-fcomte.fr>
2025-12-03 17:16:52 +0100

    lightbox

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
 // *** LIGHTBOX ***
+// Récupérer les éléments
+const lightBox = document.querySelector("#lightbox");
+const lightBoxImg = document.querySelector("#lightbox-img");
+const thumbs = document.querySelectorAll(".thumb");
+
+// 1) Clic sur une miniature => ouvrir la lightbox avec la bonne image
+thumbs.forEach((thumb) => {
+    thumb.addEventListener("click", () => {
+        const fullImgUrl = thumb.dataset.fullImg; // lit data-full-img
+        lightBoxImg.src = fullImgUrl;
+        lightBox.showModal(); // ouvre le <dialog> en modal
+    });
+});
+
+// 2) Clic sur le dialogue => le fermer
+lightBox.addEventListener("click", () => {
+    lightBox.close();
+});
+lightBox.addEventListener("click", (event) => {
+    // On ferme seulement si on clique sur le fond du dialog, pas sur l’image
+    if (event.target === lightBox) {
+        lightBox.close();
+    }
+});
+
+
